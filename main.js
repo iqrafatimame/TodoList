f50020848318c7a7b0bb521d4e3ee5db5631f7fb
Iqra Fatima <46266421+IqraFatima2000@users.noreply.github.com>
2019-11-06 23:45:49 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,42 +1,106 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
-    <title>Todo List</title>
-</head>
-<body>
-    <header></header>
-    <h1> Todo List</h1>
-    <!--<button id="displayTodosButton">Display Todos</button>
-    <button id="toggleAllButton">Toggle All </button>
-    -->
+var todoList = {
+    todos : [],
+  displayTodos : function() {
+  if(this.todos.length===0){
+          console.log('Your List is Enmpty');
+  }
+      else{
+      console.log('My Todos');
+      for(var i=0; i < this.todos.length; i++){
 
-    <div>
-        <button onclick="handlers.displayTodos()">Display Todos</button>
-        <button onclick="handlers.toggleAll()">Toggle All </button>
-    </div>
-    <div>
-        <button onclick="handlers.addTodos()">Add</button>
-        <input id="addTodoTextInput" type="text">
-    </div>
-    <div>
-        <button onclick="handlers.changeTodos()">Change Todo</button>
-        <input id="changeTodoPosition" type="number">
-        <input id="changeTodoTextInput" type="text">
-    </div>
-    <div>
-        <button onclick="handlers.deleteTodo()"> Delete Todo</button>
-        <input id="deleteTodoPosition" type="number">
-    </div>
-    <div>
-        <button onclick="handlers.toggleCompleted()" >Toggle Completed</button>
-        <input id="toggleCompletedPosition" type="number">
-    </div>
-     
-    <script src="main.js">
-    </script>
-</body>
-</html>
+          if(this.todos[i].completed===true){
+              console.log('(X)' , this.todos[i].todoText);}
+          else{
+              console.log('( )' , this.todos[i].todoText);
+          }
+        }
+      }
+  } ,
+  addTodos : function(todoText){ // addTodo('hi')
+      this.todos.push({ 
+          todoText : todoText,  // objects of todos
+          completed : false
+          });
+      this.displayTodos();
+  },
+  changeTodos : function (position,todoText){
+      this.todos[position].todoText = todoText; // will get only todoText property of the object
+      this.displayTodos();
+  },
+  deleteTodo : function(position){
+      this.todos.splice(position,1); // splice(position, number of items to be deleted);
+      this.displayTodos();  
+  },
+  toggleCompleted : function(position) {
+      var todo = this.todos[position];
+      todo.completed =!todo.completed;
+      this.displayTodos();
+  },
+  toggleAll: function(){
+  var totalTodos = this.todos.length;
+  var completedTodos = 0;
+  for(var i=0; i< totalTodos; i++){
+      if(this.todos[i].completed===true){
+          completedTodos++;
+      }
+  }
+      // case: 1 -> if everything is true , make it false
+  if(completedTodos===totalTodos){
+      for(var i=0; i<totalTodos; i++){
+          this.todos[i].completed = false;
+      }
+  }
+  else{ // case: 2 -> otherwise make everything true
+      for(var i=0; i< totalTodos; i++){
+          this.todos[i].completed = true;}
+      }
+          this.displayTodos();
+ 
+}
+      
+};
+    // in this way we had to write extra code to access the button 
+/*
+// 1: we want to get access to the button
+var displayTodosButton = document.getElementById('displayTodosButton');
+console.log(displayTodosButton); 
+var toggleAllButton=document.getElementById('toggleAllButton');
+
+    // 2: to perform the function when clicked
+displayTodosButton.addEventListener('click', function(){
+    todoList.displayTodos();
+});
+toggleAllButton.addEventListener('click',function(){
+    todoList.toggleAll();
+});*/
+
+var handlers={                  // to simplify our code we used a object to access the button with specific method
+    displayTodos:  function(){
+        todoList.displayTodos();
+    },
+    toggleAll:  function(){
+        todoList.toggleAll()
+    },
+    addTodos:  function(){
+        var addTodoTextInput=document.getElementById('addTodoTextInput');
+        todoList.addTodos(addTodoTextInput.value);
+        addTodoTextInput.value='';
+    },
+    changeTodos:  function(){
+        var changeTodoPosition=document.getElementById('changeTodoPosition');
+        var changeTodoTextInput=document.getElementById('changeTodoTextInput');
+        todoList.changeTodos(changeTodoPosition.valueAsNumber,changeTodoTextInput.value);
+        changeTodoPosition.value='';
+        changeTodoTextInput.value='';
+    },
+    deleteTodo:  function(){
+        var deleteTodoPosition=document.getElementById('deleteTodoPosition');
+        todoList.changeTodos(deleteTodoPosition.valueAsNumber);
+        deleteTodoPosition.value='';
+    },
+    toggleCompleted:  function(){
+        var toggleCompletedPosition= document.getElementById('toggleCompletedPosition');
+        todoList.toggleCompleted(toggleCompletedPosition.valueAsNumber);
+        toggleCompletedPosition.value='';
+    }
+};
